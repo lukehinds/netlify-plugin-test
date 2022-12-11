ab925d355c7561c57c703f911ecd0eeb7db80534
Luke Hinds <lhinds@redhat.com>
2022-12-11 16:08:09 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ export const onPreBuild = async function ({
 //   status.show({ summary: 'Success!' })
 // }
       try {
-            const response = await fetch('https://api.netlify.com/api/v1/sites');
+            const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
             const json = await response.json();
             console.log(json);
             // write json to a file called sites.json
